47d91b86416cd602d39bead78e62f5c03df41f18
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-25 13:58:22 +0800

    Update info-ytbot.js

--- plugins/info-ytbot.js
+++ plugins/info-ytbot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `YT: FANGZ BOT\n`,wm + '\n\n' + botdate, giflogo, [['OK','ok']], m, {
+ await conn.sendButton(m.chat, `YT: FIN BOT\n`,wm + '\n\n' + botdate, giflogo, [['OK','ok']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
@@ -11,7 +11,7 @@ contextInfo: { externalAdReply :{
                         title: bottime,
                         body: 'By Fangz',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: 'https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg'
+                        sourceUrl: 'https://youtube.com/channel/UCCZFKpk85uGzGdGk9kUZUIA'
                       }}
 })
 }
