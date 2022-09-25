75d98d37b977a6546c40762aec3b35c4befdc60f
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-25 11:11:08 +0800

    Update _namecreator.js

--- plugins/_namecreator.js
+++ plugins/_namecreator.js
@@ -13,13 +13,13 @@ const message = {
                 {
                     urlButton: {
                         displayText: '🌹YT OWNER',
-                        url: 'https://s.id/-1a7RO',
+                        url: 'https://youtube.com/channel/UCCZFKpk85uGzGdGk9kUZUIA',
                     }
                 },
                 {
                     urlButton: {
                         displayText: '📞 Author',
-                        url: 'https://wa.me/6288215689772',
+                        url: 'https://wa.me/6282261065144',
                     }
                 },
                 {
