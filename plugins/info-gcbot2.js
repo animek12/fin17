e3e5091bcc82fce149bd0c6c8613f481fd5c1b5f
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-25 14:00:38 +0800

    Update info-gcbot2.js

--- plugins/info-gcbot2.js
+++ plugins/info-gcbot2.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.wm
 let str = `
-✧─────[ *Group TokoBot* ]─────✧
+✧─────[ *Group share link* ]─────✧
 🍀 Group 2 :
 https://linktr.ee/fin222
 ✧──────────···──────────✧
