0e61fb208901e297f0d02aa9483ffa9a92d29b80
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-25 13:54:22 +0800

    Update info-gcbot.js

--- plugins/info-gcbot.js
+++ plugins/info-gcbot.js
@@ -4,7 +4,7 @@ let botol = global.wm
 let str = `
 ✧─────[ *Group Official* ]─────✧
 🍀 Group 1 :
-https://chat.whatsapp.com/HpxyS6b45SoGIKG7LHddiM
+https://linktr.ee/fin222
 ✧──────────···──────────✧
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, [['⋮☰ MENU','.menu']],m)
