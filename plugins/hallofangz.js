01998679ca4a3635f768a9647f98f4066bbfede1
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-25 13:41:12 +0800

    Update hallofangz.js

--- plugins/hallofangz.js
+++ plugins/hallofangz.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 `.trim()
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak Shinn Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak Alfin Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
 }
-handler.customPrefix = /^(Hallo Fangzz|Hallo fangz|hi fangz|Hi Fangz)$/i
+handler.customPrefix = /^(Hallo Fin|Halo fin|hi fin|Hi Fin)$/i
 handler.command = new RegExp
 
 handler.owner = false
